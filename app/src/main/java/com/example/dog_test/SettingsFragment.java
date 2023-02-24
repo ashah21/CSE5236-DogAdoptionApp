08dda268ed0896e281ce4c103766d258d4ea2898
Franz Lleshaj <66387518+franzlleshaj@users.noreply.github.com>
2023-02-24 17:39:30 -0500

    Added logging for onCreateView.

--- app/src/main/java/com/example/dog_test/SettingsFragment.java
+++ app/src/main/java/com/example/dog_test/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.example.dog_test;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,12 @@ import androidx.fragment.app.Fragment;
 
 public class SettingsFragment extends Fragment {
 
+    private static final String TAG = "SettingsActivity";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView() called");
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
