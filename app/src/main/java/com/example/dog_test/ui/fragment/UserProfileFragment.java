e8ee7e958af542fb5a2a3bcdb3102da76e22b2c0
ashah21 <anya.shah21@gmail.com>
2023-04-10 11:25:14 -0400

    checking if camera opens when btn clicked

--- app/src/main/java/com/example/dog_test/ui/fragment/UserProfileFragment.java
+++ app/src/main/java/com/example/dog_test/ui/fragment/UserProfileFragment.java
@@ -2,17 +2,25 @@ package com.example.dog_test.ui.fragment;
 
 import static android.app.appsearch.AppSearchResult.RESULT_OK;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,9 +32,11 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.dog_test.R;
+import com.example.dog_test.databinding.FragmentUserProfileBinding;
 import com.example.dog_test.ui.activity.MainActivity;
 import com.example.dog_test.ui.activity.RegisterActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
@@ -39,7 +49,10 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 public class UserProfileFragment extends Fragment{
@@ -52,6 +65,13 @@ public class UserProfileFragment extends Fragment{
     Button btnCamera;
     FirebaseUser user;
     ProgressBar progressBar;
+
+    // for using camera to take a pic
+    FragmentUserProfileBinding profileBinding;
+    ActivityResultLauncher<Uri> takePictureLauncher;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Uri imageUri;
+    private static final int CAMERA_PERMISSION_CODE = 1;
 
 
     @Nullable
@@ -66,17 +86,28 @@ public class UserProfileFragment extends Fragment{
         btnCamera = (Button) view.findViewById(R.id.btn_camera);
 //        camera.setOnClickListener(this);
 
-        user = FirebaseAuth.getInstance().getCurrentUser();
+//        user = FirebaseAuth.getInstance().getCurrentUser();
+
         profilePic = view.findViewById(R.id.user_profile);
         progressBar = view.findViewById(R.id.progressBar);
-        storage = FirebaseStorage.getInstance();
-        storageReference = storage.getReference();
 
-//        if(user == null){
-//            Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
-//            getActivity().startActivity(intent);
-//            getActivity().finish();
-//        }
+//        storage = FirebaseStorage.getInstance();
+//        storageReference = storage.getReference();
+
+        // camera
+//        profileBinding = FragmentUserProfileBinding.inflate(getLayoutInflater());
+        //imageUri = createUri();
+        //registerPictureLauncher();
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //checkCameraPermissionAndOpenCamera();
+//                takePictureIntent();
+
+                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(open_camera, 0);
+            }
+        });
 
 
         profilePic.setOnClickListener(new View.OnClickListener() {
@@ -100,13 +131,74 @@ public class UserProfileFragment extends Fragment{
         return view;
     }
 
-//    private void choosePicture() {
-//        Intent intent = new Intent();
-//        intent.setType("image/*");
-//        intent.setAction(Intent.ACTION_GET_CONTENT);
-//        startActivityForResult(intent, 1);
-//    }
-//
+    private void takePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Handle error
+            }
+            if (photoFile != null) {
+                imageUri = FileProvider.getUriForFile(getActivity(),
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        return imageFile;
+    }
+
+
+
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference imageRef = storageRef.child("images/" + imageUri.getLastPathSegment());
+        UploadTask uploadTask = imageRef.putFile(imageUri);
+
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // Image upload success, get the download URL and save it to Firebase Realtime Database or Firestore
+                imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // Save the download URL to Firebase Realtime Database or Firestore
+                        // ...
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Image upload failed, handle the error
+                // ...
+            }
+        });
+    }
+
+        private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
         if (resultCode == Activity.RESULT_OK) {
@@ -121,6 +213,8 @@ public class UserProfileFragment extends Fragment{
                 }
                 profilePic.setImageBitmap(bitmapImage);
             }
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            uploadImageToFirebase(imageUri);
         }
 
         imagePath = data.getData();
@@ -160,7 +254,7 @@ public class UserProfileFragment extends Fragment{
                     });
                     Toast.makeText( getActivity().getApplicationContext(), "Image Uploaded!", Toast.LENGTH_LONG).show();
                 }else{
-                    Toast.makeText( getActivity().getApplicationContext(), task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                    Toast.makeText( getActivity().getApplicationContext(), "Image not uploaded", Toast.LENGTH_LONG).show();
                 }
             }
         }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
@@ -176,7 +270,55 @@ public class UserProfileFragment extends Fragment{
         FirebaseDatabase.getInstance().getReference("users/"+FirebaseAuth.getInstance().getCurrentUser().getUid() + "/userImage").setValue(url);
     }
 
+    private Uri createUri(){
+        File imageFile = new File(getActivity().getApplicationContext().getFilesDir(), "camera_photo.jpg");
+        return FileProvider.getUriForFile(
+                getActivity().getApplicationContext(),
+                "com.example.camerapermission.fileProvider",
+                imageFile
+        );
+    }
 
+    private void registerPictureLauncher(){
+        takePictureLauncher = registerForActivityResult(
+                new ActivityResultContracts.TakePicture(),
+                new ActivityResultCallback<Boolean>(){
+                    @Override
+                    public void onActivityResult(Boolean result){
+                        try {
+                            if(result){
+                                profileBinding.userProfile.setImageURI(null);
+                                profileBinding.userProfile.setImageURI(imageUri);
+                            }
+                        }catch (Exception exception){
+                            exception.getStackTrace();
+                        }
+                    }
+                }
+        );
+    }
+
+    private void checkCameraPermissionAndOpenCamera(){
+        if(ActivityCompat.checkSelfPermission(getActivity().getApplication(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }else{
+            takePictureLauncher.launch(imageUri);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permission, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permission, grantResults);
+        if(requestCode == CAMERA_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                takePictureLauncher.launch(imageUri);
+            }else{
+                Toast.makeText(getActivity().getApplicationContext(), "Camera Permission denied, please allow permission to take picture", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
 
 }
