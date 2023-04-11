362c91c6aff88fd99101e0f77b01d7e34f30a86a
ashah21 <anya.shah21@gmail.com>
2023-04-11 14:23:52 -0400

    Changed input type for passwords for security purposes so password is not visible

--- app/src/main/java/com/example/dog_test/ui/fragment/UserProfileFragment.java
+++ app/src/main/java/com/example/dog_test/ui/fragment/UserProfileFragment.java
@@ -5,9 +5,11 @@ import static android.app.appsearch.AppSearchResult.RESULT_OK;
 import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -23,6 +25,7 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -103,12 +106,25 @@ public class UserProfileFragment extends Fragment{
         //imageUri = createUri();
         //registerPictureLauncher();
 
+
         ActivityResultLauncher takePictureLauncher = registerForActivityResult(new ActivityResultContracts.TakePicturePreview(), new ActivityResultCallback<Bitmap>() {
             @Override
             public void onActivityResult(Bitmap result) {
-
+//                profilePic.setImageBitmap(result);
+//                Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                cameraIntent.setType("image/*");
+//                Bitmap bitmap = null;
+//                try {
+//                    bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),
+//                            cameraIntent.getData());
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                profilePic.setImageBitmap(bitmap);
+                Log.i("Result", result.toString());
             }
         });
+
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -277,7 +293,6 @@ public class UserProfileFragment extends Fragment{
             }
         }
     }
-
 
 
 
