94b323ac0038a70e6b5653b0e18541bb4ac14b58
ashah21 <anya.shah21@gmail.com>
2023-04-13 20:48:11 -0400

    Made some UI changes: font bigger and added a title to login and register page

--- app/src/test/java/com/example/dog_test/ui/activity/LoginActivityTest.java
+++ app/src/test/java/com/example/dog_test/ui/activity/LoginActivityTest.java
@@ -2,19 +2,25 @@ package com.example.dog_test.ui.activity;
 
 import static org.junit.Assert.*;
 
-import android.app.Activity;
+import android.view.View;
 
 import com.google.errorprone.annotations.DoNotMock;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 
-public class LoginActivityTest {
+
+public class LoginActivityTest{
+
+    private LoginActivity mLoginActivity;
 
     @Before
     public void setUp() throws Exception {
+        mLoginActivity = new LoginActivity();
     }
+
 
 
     @Test
@@ -27,11 +33,27 @@ public class LoginActivityTest {
         mTest.editTextEmail.setText("suzy@gmail.com");
         mTest.editTextPassword.setText("password");
         mTest.buttonLogin.performClick();
+        mTest.mAuth.signInWithEmailAndPassword("suzy@gmail.com", "password");
 
         assertEquals("Valid login", mTest.testMsg);
 
 
     }
+
+    @Test
+    public void testLogin(){
+//        mLoginActivity = new LoginActivity();
+        mLoginActivity.onStart();
+        mLoginActivity.editTextEmail.setText("suzy@gmail.com");
+        mLoginActivity.editTextPassword.setText("password");
+        mLoginActivity.buttonLogin.performClick();
+        if(mLoginActivity != null){
+            View loginActivityView = mLoginActivity.getCurrentFocus();
+        }
+
+        assertNotNull(mLoginActivity);
+    }
+
 
 
 }
