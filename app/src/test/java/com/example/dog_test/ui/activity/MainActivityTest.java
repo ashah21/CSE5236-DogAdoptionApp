d0dab41053f30a3ae943cdb2711f4be930cd1035
ashah21 <anya.shah21@gmail.com>
2023-04-13 23:02:42 -0400

    Minor UI changes

---
+++ app/src/test/java/com/example/dog_test/ui/activity/MainActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.dog_test.ui.activity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Test
+    public void navigationItemSelectedTest(){
+        MainActivity mainActivity = new MainActivity();
+
+//        assertTrue(mainActivity.onNavigationItemSelected(R.id.home));
+
+    }
+
+}
